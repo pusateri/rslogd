9f90c1069e82e03355202b935334ec9288d8489c
Tom Pusateri <pusateri@bangj.com>
2019-06-09 23:42:04 -0400

    Handle stage1 comments from @thomasdezeeuw in #4. Thanks!

--- src/main.rs
+++ src/main.rs
@@ -9,48 +9,55 @@
 //! # panics
 //!
 //! If socket cannot bind to syslog UDP port 514 (permissions or already in use)
+//! sudo allows the program to bind to a port < 1024
 //!
-
 use mio::net::UdpSocket;
 use mio::{Events, Poll, PollOpt, Ready, Token};
+use std::io::ErrorKind;
 
 const SERVER: Token = Token(0);
 
-fn main() {
-    let mut events = Events::with_capacity(1024);
-    let poll = Poll::new().expect("Poll::new() failed");
+fn main() -> Result<(), std::io::Error> {
+    let mut events = Events::with_capacity(256);
+    let poll = Poll::new()?;
     let mut buffer = [0u8; 4096];
 
     // create listen socket
-    let udp_server_socket = match UdpSocket::bind(&"127.0.0.1:514".parse().expect("parse failed")) {
-        Ok(new_socket) => new_socket,
-        Err(fail) => {
-            panic!("Failed to bind socket. {:?}", fail);
-        }
-    };
+    let udp_server_socket = UdpSocket::bind(&"127.0.0.1:514".parse().expect("parse failed"))?;
     // tell mio about the socket
     poll.register(
         &udp_server_socket,
         SERVER,
         Ready::readable(),
         PollOpt::level(),
-    )
-    .expect("poll.register failed");
+    )?;
 
     // main event loop
-    loop {
+    let mut shutdown = false;
+    while !shutdown {
         // wait for an event to occur
-        poll.poll(&mut events, None).expect("poll.poll failed");
+        poll.poll(&mut events, None)?;
         for event in events.iter() {
             // find token of matching event
             match event.token() {
                 SERVER => {
                     // read from the socket
-                    let len = udp_server_socket.recv(&mut buffer).expect("recv errors");
-                    println!("recv {} bytes", len);
+                    match udp_server_socket.recv(&mut buffer) {
+                        Ok(len) => println!("recv {} bytes", len),
+                        Err(e) => {
+                            if e.kind() == ErrorKind::WouldBlock
+                                || e.kind() == ErrorKind::Interrupted
+                            {
+                                continue;
+                            } else {
+                                shutdown = true;
+                            }
+                        }
+                    }
                 }
                 _ => (),
             }
         }
     }
+    Ok(())
 }
